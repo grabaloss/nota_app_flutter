e403e65011b306f0dcdb838c1c013388285beb8a
grabaloss <128254322+grabaloss@users.noreply.github.com>
2025-11-21 16:49:31 -0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\dgrab\\Downloads\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\dgrab\\Downloads\\notas_app-main\\notas_app-main" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\dgrab\\Downloads\\flutter"
+  "PROJECT_DIR=C:\\Users\\dgrab\\Downloads\\notas_app-main\\notas_app-main"
+  "FLUTTER_ROOT=C:\\Users\\dgrab\\Downloads\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\dgrab\\Downloads\\notas_app-main\\notas_app-main\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\dgrab\\Downloads\\notas_app-main\\notas_app-main"
+  "FLUTTER_TARGET=C:\\Users\\dgrab\\Downloads\\notas_app-main\\notas_app-main\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzUuNw==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049YWRjOTAxMDYyNQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MDM1MzE2NTY1YQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjI="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\dgrab\\Downloads\\notas_app-main\\notas_app-main\\.dart_tool\\package_config.json"
+)
